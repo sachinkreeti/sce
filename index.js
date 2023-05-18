b0fbea957fe353ab0714701e464359999f5f3fc1
sachinkreeti <sgupta@kreeti.com>
2023-05-18 12:39:35 +0530

    explains appendChild

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ var heading = document.getElementById('heading');
 console.log('heading', heading);
 heading.innerHTML = 'Hello JS';
 
-// var secondChild = document.getElementById('second-child');
-// secondChild.parentNode // div
-// secondChild.parentNode.parentNode // body
+// parentNode
+var secondChild = document.getElementById('second-child');
+secondChild.parentNode // div
+secondChild.parentNode.parentNode // body
+
+// getElementByTagName
+var pList = document.getElementsByTagName('p') // returns a collection
+pList[2] // returns the third element
+pList[2].innerHTML = 'third para changed'
+
+// creating a new element and adding it to the DOM
+function myFunction() {
+  // Create an "li" node:
+  const node = document.createElement("li");
+  node.innerHTML = 'Water'
+
+  // Append the "li" node to the list:
+  document.getElementById("myList").appendChild(node);
+  }
+
