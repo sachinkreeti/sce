df208b42863b1bbe4acfe4c0c754f603b85514f1
sachinkreeti <sgupta@kreeti.com>
2023-05-18 12:34:37 +0530

    explains how to use parentNode

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 var heading = document.getElementById('heading');
 console.log('heading', heading);
 heading.innerHTML = 'Hello JS';
+
+// var secondChild = document.getElementById('second-child');
+// secondChild.parentNode // div
+// secondChild.parentNode.parentNode // body
