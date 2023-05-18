8a77f47cf552adac721cef04d84a2bf91dea17dd
sachinkreeti <sgupta@kreeti.com>
2023-05-18 12:50:20 +0530

    add notes

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-var heading = document.getElementById('heading');
-console.log('heading', heading);
-heading.innerHTML = 'Hello JS';
+// Accessing an element
+// you can use document.getElementById, getElementByClassName, getElementByTagName
+// you can also use querySelector and querySelectorAll
+// LINK TO FOLLOW: https://www.codecademy.com/learn/fscp-building-interactive-websites-with-javascript/modules/fecp-javascript-and-the-dom/cheatsheet
 
-// parentNode
+// Modifying an element
+// An element/node is represented as object. So basically to modify something, you have to find the right key and modify the value for that key. Key is also called property.
+
+// Adding an element
+// you can use append* related methods to add a element
+
+// Traversing DOM
+// parentNode (traversing up the DOM tree)
 var secondChild = document.getElementById('second-child');
 secondChild.parentNode // div
 secondChild.parentNode.parentNode // body
+
+// to access children of a parent (traversing down the DOM tree)
+element.children
+
+var heading = document.getElementById('heading');
+console.log('heading', heading);
+heading.innerHTML = 'Hello JS';
 
 // getElementByTagName
 var pList = document.getElementsByTagName('p') // returns a collection
@@ -20,5 +35,4 @@ function myFunction() {
 
   // Append the "li" node to the list:
   document.getElementById("myList").appendChild(node);
-  }
-
+}
